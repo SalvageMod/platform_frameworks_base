2677e91357e98a319d044a9ba8f35204ce350202
Alex Avance <aravance@gmail.com>
2011-03-29 12:07:53 -0500

    Add an option to change the device hostname (1/2).

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2391,6 +2391,11 @@ public final class Settings {
         public static final String ADB_ENABLED = "adb_enabled";
 
         /**
+         * The host name for this device.
+         */
+        public static final String DEVICE_HOSTNAME = "device_hostname";
+
+        /**
          * Setting to allow mock locations and location provider status to be injected into the
          * LocationManager service for testing purposes during application development.  These
          * locations and status values  override actual location and status information generated
