a96091f889a00d2e5499cb440cd39c9b15753fc4
cyanogen <shade@chemlab.org>
2011-06-16 11:48:46 -0400

    stagefright: Only use stride/slice height for decode on MSM/QSD
    Use the old behavior unless on MSM/QSD as some hardware expects it.
    
    See commit f51d295

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -106,6 +106,10 @@ ifeq ($(TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
         LOCAL_LDLIBS += -lpthread
 endif
 
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+        LOCAL_CFLAGS += -DUSE_QCOM_OMX_FIX
+endif
+
 LOCAL_CFLAGS += -Wno-multichar
 
 LOCAL_MODULE:= libstagefright
